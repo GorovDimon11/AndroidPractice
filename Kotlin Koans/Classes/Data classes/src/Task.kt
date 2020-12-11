afb06e79e2acc872134d1efcadc9d6eb7ca9b99b
DMYTRO <GorovDimon11>
2020-12-11 02:22:42 +0200

    Added solution for several tasks

--- Kotlin Koans/Classes/Data classes/src/Task.kt
+++ Kotlin Koans/Classes/Data classes/src/Task.kt
@@ -1,4 +1,9 @@
-class Person
+data class Person(
+    val name: String,
+    val age: Int
+)
+
+
 
 fun getPeople(): List<Person> {
     return listOf(Person("Alice", 29), Person("Bob", 31))
@@ -7,5 +12,5 @@ fun getPeople(): List<Person> {
 fun comparePeople(): Boolean {
     val p1 = Person("Alice", 29)
     val p2 = Person("Alice", 29)
-    return p1 == p2  // should be true
+    return p1.equals(p2)  // should be true
 }
