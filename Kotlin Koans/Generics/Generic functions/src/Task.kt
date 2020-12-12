c3482caea8f362d90f252a632790d940d1f8871e
DMYTRO <GorovDimon11>
2020-12-12 16:12:02 +0200

    Added solution for several tasks

--- Kotlin Koans/Generics/Generic functions/src/Task.kt
+++ Kotlin Koans/Generics/Generic functions/src/Task.kt
@@ -1,6 +1,17 @@
 import java.util.*
+import kotlin.collections.ArrayList
+import kotlin.collections.HashSet
 
-fun partitionTo() = TODO()
+fun <T, C : MutableCollection<T>> Collection<T>.partitionTo(first: C, second: C, predicate: (T) -> Boolean): Pair<C, C> {
+    for (element in this) {
+        if (predicate(element)) {
+            first.add(element)
+        } else {
+            second.add(element)
+        }
+    }
+    return Pair(first, second)
+}
 
 fun partitionWordsAndLines() {
     val (words, lines) = listOf("a", "a b", "c", "d e")
--- Kotlin Koans/Introduction/Named arguments/src/Task.kt
+++ Kotlin Koans/Introduction/Named arguments/src/Task.kt
@@ -1,3 +1,14 @@
 fun joinOptions(options: Collection<String>) =
         options.joinToString(prefix = "[",
         separator = ", ", postfix = "]")
+
+/*
+fun bar(i: Int, s: String = "", b: Boolean = true) {}
+
+fun usage() {
+    // named arguments
+    bar(1, b = false)
+}
+
+fun task2(collection: Collection<Int>) =
+        collection.joinToString(prefix = "{", postfix = "}")*/
